ba6351f549db7c3ca4700fec52693a847b666ccb
Norton794 <nortonsantos79@gmail.com>
2022-03-22 21:47:29 -0300

    static about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,21 @@
-import Navbar from "../components/Navbar"
-export default function About(){
-    return(
-        <div>
-            <Navbar/>
-            <h1>Sobre</h1>
-            <h2>Sobre Nós:</h2>
-            <p>Et esse cillum labore nostrud esse voluptate ad magna occaecat excepteur exercitation id. Ea occaecat proident Lorem incididunt tempor cillum. Lorem sunt sint quis est officia qui eu do. Dolor ad nostrud in ad fugiat et occaecat veniam elit proident laboris. Laborum deserunt enim labore occaecat consequat ea deserunt dolor aliquip.</p>
-            </div>
-    )
+import Navbar from "../components/Navbar";
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-center  text-gray-350 uppercase font-sans font-semibold text-xl mt-8">
+        Sobre
+      </h1>
+      <div className="p-4 m-4">
+        <h2 className="capitalize font-sans font-semibold mt-2 mb-2 text-lg text-gray-350">Sobre Nós:</h2>
+        <p className="indent-8 text-gray-350">
+          Et esse cillum labore nostrud esse voluptate ad magna occaecat
+          excepteur exercitation id. Ea occaecat proident Lorem incididunt
+          tempor cillum. Lorem sunt sint quis est officia qui eu do. Dolor ad
+          nostrud in ad fugiat et occaecat veniam elit proident laboris. Laborum
+          deserunt enim labore occaecat consequat ea deserunt dolor aliquip.
+        </p>
+      </div>
+    </div>
+  );
 }
