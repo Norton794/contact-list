36b31a39a3b3c098b673d11ba203aa6cdb9bd8e5
Norton794 <nortonsantos79@gmail.com>
2022-03-23 15:12:58 -0300

    posting with axios

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,67 +1,73 @@
 import { IconSave } from "./Icons";
 
-export default (props) => (
-  <form className="w-full  flex justify-center">
-    <div className="flex flex-wrap mt-8 p-3">
-      <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mt-4">
-        <label
-          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="grid-first-name"
-        >
-          Nome
-        </label>
-        <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="grid-first-name"
-          type="text"
-          placeholder="Nome"
-          required
-        />
+export default ({nome, telefone, description, add, changeNome, changeTelefone, changeDesc}) => {
+
+
+
+  return (
+    <div className="w-full  flex justify-center">
+      <div className="flex flex-wrap mt-8 p-3">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mt-4">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-first-name"
+          >
+            Nome
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            value={nome}
+            type="text"
+            placeholder="Nome"
+            onChange={e=>changeNome(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mt-4">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-first-name"
+          >
+            Telefone
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            value={telefone}
+            type="text"
+            placeholder="(xx)xxxxx-xxxx"
+            onChange={e=>changeTelefone(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="w-full px-3 mt-4">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-first-name"
+          >
+            Descrição
+          </label>
+          <textarea
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white h-32"
+            type="text"
+            value={description}
+            onChange={e=>changeDesc(e.target.value)}
+            required
+          ></textarea>
+        </div>
+
+        <div className="flex items-center justify-center w-full">
+          <div className="">
+            <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded flex"
+            onClick={add}
+            >
+              <span className="mr-2">{IconSave}</span> 
+              Cadastrar
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 mt-4">
-        <label
-          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="grid-first-name"
-        >
-          Telefone
-        </label>
-        <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="grid-first-name"
-          type="text"
-          placeholder="(xx)xxxxx-xxxx"
-          required
-        />
-      </div>
-
-
-      <div className="w-full px-3 mt-4">
-        <label
-          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-          for="grid-first-name"
-        >
-          Descrição
-        </label>
-        <textarea
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white h-32"
-
-          type="text"
-          required
-        ></textarea>
-      </div>
-
-<div className="flex items-center justify-center w-full">
-    
-<div className="">
-      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded flex">
-       <span className="mr-2">{IconSave}</span> Cadastrar
-      </button>
     </div>
-</div>
-      
-
-
-    </div>
-  </form>
-);
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,43 @@
-import Form from "../components/Form"
-import Navbar from "../components/Navbar"
-import Table from "../components/Table"
-
+import Form from "../components/Form";
+import Navbar from "../components/Navbar";
+import Table from "../components/Table";
+import { useState } from "react";
+import { URL } from "../utils";
+import axios from "axios";
 export default function Home() {
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [description, setDescription] = useState("");
+const [list, setList] = useState([]);
+  const refresh = () => {
+    axios.get(`${URL}?sort=-name`)
+    .then(resp=>setList(resp))
+  }
+
+  const addContact = () => {
+    axios
+      .post(URL, { name: nome, description, phone: telefone })
+      .then((resp) => {
+        console.log("Foi");
+        setNome("");
+        setTelefone("");
+        setDescription("");
+      })
+      .catch((err) => console.error("Erro ao Salvar", err));
+  };
   return (
     <div className={``}>
-      <Navbar/>
-      <Table/>
-      <Form/>
+      <Navbar />
+      <Table list={list} />
+      <Form
+        nome={nome}
+        telefone={telefone}
+        description={description}
+        add={addContact}
+        changeNome={setNome}
+        changeTelefone={setTelefone}
+        changeDesc={setDescription}
+      />
     </div>
-  )
+  );
 }
