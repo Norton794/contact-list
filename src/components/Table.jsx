452802ca65832b150e3efab273c066ec7a636cf5
Norton794 <nortonsantos79@gmail.com>
2022-03-23 15:30:47 -0300

    fetching from the api with axios

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,6 +4,22 @@ export default function Table(props){
     return(
         <div>
             <Header text="Telefones: "/>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Telefone</th>
+                    <th>Descrição</th>
+                </tr>
+                {props.list && props.list.map(contact=>(
+                    <tr key={contact._id} className="odd:bg-white even:bg-slate-100">
+                        <td>{contact._id}</td>
+                        <td>{contact.name}</td>
+                        <td>{contact.phone}</td>
+                        <td>{contact.description}</td>
+                    </tr>
+                ))}
+            </table>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,32 @@
 import Form from "../components/Form";
 import Navbar from "../components/Navbar";
 import Table from "../components/Table";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { URL } from "../utils";
 import axios from "axios";
 export default function Home() {
   const [nome, setNome] = useState("");
   const [telefone, setTelefone] = useState("");
   const [description, setDescription] = useState("");
-const [list, setList] = useState([]);
+  const [list, setList] = useState([]);
+
   const refresh = () => {
-    axios.get(`${URL}?sort=-name`)
-    .then(resp=>setList(resp))
-  }
+    axios.get(`${URL}?sort=-createdAt`).then((resp) => {
+      setList(resp.data);
+      console.log(list);
+    })
+    .catch(err=>console.log('Error', err))
+  };
+
+  useEffect(() => {
+    refresh()
+  }, [nome, telefone, description]);
 
   const addContact = () => {
     axios
       .post(URL, { name: nome, description, phone: telefone })
       .then((resp) => {
-        console.log("Foi");
+        refresh()
         setNome("");
         setTelefone("");
         setDescription("");
